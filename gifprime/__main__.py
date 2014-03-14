55871d51baf357aa1a26ca145c664cbe69f8b1c5
Tom Dryer <tomdryer.com@gmail.com>
2014-03-13 21:15:39 -0700

    Add the beginnings of the public interface for GIF

--- gifprime/__main__.py
+++ gifprime/__main__.py
@@ -1,12 +1,35 @@
 import gifprime.parser
 
 
+class Image(object):
+    """A single image from a GIF."""
+
+    def __init__(self):
+        self.image_data = None
+        self.user_input_flag = False
+        self.delay_time = 0
+        # TODO: likely we can abstract these away:
+        self.transparent_colour = None
+        self.disposal_method = None
+
+
 class GIF(object):
+    """A GIF image or animation."""
 
-    def __init__(self, filename):
-        with open(filename, 'rb') as f:
-            data_stream = f.read()
-        parsed_data = gifprime.parser.gif.parse(data_stream)
-        #print parsed_data
-        #assert False
+    def __init__(self, filename=None):
+        """Create a new GIF or decode one from a file."""
+        self.images = []
+        self.comments = []
+        self.filename = filename
 
+        if filename != None:
+            with open(filename, 'rb') as f:
+                data_stream = f.read()
+            parsed_data = gifprime.parser.gif.parse(data_stream)
+            self.comments = [parsed_data.comment_extension.comment]
+            #print parsed_data
+            #assert False
+
+    def save(self, filename):
+        """Encode a GIF and save it to a file."""
+        raise NotImplementedError
--- gifprime/test/test_decoder.py
+++ gifprime/test/test_decoder.py
@@ -2,4 +2,7 @@ from gifprime.__main__ import GIF
 
 
 def test_decoder():
-    gif = GIF('gifprime/test/data/whitepixel.gif')
+    filename = 'gifprime/test/data/whitepixel.gif'
+    gif = GIF(filename)
+    assert gif.filename == filename
+    assert gif.comments == ["Created with GIMP"]
