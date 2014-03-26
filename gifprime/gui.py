69c1812a4080fd583ce79cbb16da9e8e7737cff3
Michael Tom-Wing <mtomwing@gmail.com>
2014-03-25 20:09:27 -0700

    Added a LazyFrames generator-like object

--- gifprime/gui.py
+++ gifprime/gui.py
@@ -8,6 +8,49 @@ from gifprime.__main__ import GIF
 pygame.init()
 
 
+class LazyFrames(object):
+    """Lazy GIF image 'generator'."""
+
+    def __init__(self, gif):
+        self.gif = gif
+        self.surfaces = {}
+        self.current = None
+
+    def __iter__(self):
+        while True:
+            yield self.next()
+
+    def __reversed__(self):
+        while True:
+            yield self.prev()
+
+    def get_surface(self, i):
+        """Gets the PyGame Surface corresponding to image[i] and its delay."""
+        if i not in self.surfaces:
+            image = self.gif.images[i]
+            data = ''.join(''.join(chr(c) for c in pixel)
+                           for pixel in image.rgba_data)
+            self.surfaces[i] = pygame.image.fromstring(data, gif.size, 'RGBA')
+
+        return self.surfaces[i], self.gif.images[i].delay_ms
+
+    def next(self):
+        """Returns the next (surface, delay)."""
+        if self.current is None:
+            self.current = -1
+
+        self.current = (self.current + 1) % len(self.gif.images)
+        return self.get_surface(self.current)
+
+    def prev(self):
+        """Returns the previous (surface, delay)."""
+        if self.current is None:
+            self.current = len(self.gif.images)
+
+        self.current = (self.current - 1) % len(self.gif.images)
+        return self.get_surface(self.current)
+
+
 class GIFViewer(object):
     FPS = 1
 
