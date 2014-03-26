7651d74c0daec66bf3101a3dce0eb46c447351d3
Michael Tom-Wing <mtomwing@gmail.com>
2014-03-25 20:10:42 -0700

    GIFViewer now supports delay_ms from the individual GIF frames

--- gifprime/gui.py
+++ gifprime/gui.py
@@ -52,44 +52,57 @@ class LazyFrames(object):
 
 
 class GIFViewer(object):
-    FPS = 1
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.screen = pygame.display.set_mode((width, height))
+    def __init__(self, gif, width=None, height=None, fps=60):
+        self.gif = gif
+        self.fps = fps
+
+        self.frames = LazyFrames(gif)
+        self.frames_iter = iter(self.frames)
+        self.frame_delay = 0
+        self.ms_since_last_frame = 0
+
+        # TODO: Automatically pick a good width/height
+        self.width = 300
+        self.height = 300
+
+        # Setup pygame stuff
+        self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
 
-        self.gif = GIF(sys.argv[1])
-
-        self.surfaces = [pygame.image.fromstring(''.join(''.join(chr(c) for c in pixel)
-                                                         for pixel in image.rgba_data),
-                                                 self.gif.size, 'RGBA')
-                         for image in self.gif.images]
-        self.surfaces_iter = itertools.cycle(self.surfaces)
-
-    def __do_events(self):
+    def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
 
-    def __do_draw(self, elapsed):
-        self.screen.blit(next(self.surfaces_iter), (0, 0))
+    def handle_draw(self, elapsed):
+        self.ms_since_last_frame += elapsed
+
+        if self.ms_since_last_frame >= self.frame_delay:
+            frame, self.frame_delay = next(self.frames_iter)
+            self.ms_since_last_frame = 0
+            self.screen.blit(frame, (0, 0))
+
         pygame.display.flip()
 
-    def main(self):
+    def show(self):
         now = 0
         show_fps = 0
+
         while True:
             elapsed = pygame.time.get_ticks() - now
             now = pygame.time.get_ticks()
-            self.__do_draw(elapsed)
-            self.__do_events()
-            self.clock.tick(self.FPS)
+
+            self.handle_draw(elapsed)
+            self.handle_events()
+            self.clock.tick(self.fps)
+
             show_fps = show_fps + 1
-            if (show_fps % self.FPS == 0):
+            if show_fps % self.fps == 0:
                 print self.clock.get_fps()
 
+
 if __name__ == '__main__':
-    viewer = GIFViewer(300, 300)
-    viewer.main()
+    gif = GIF(sys.argv[1])
+    viewer = GIFViewer(gif, fps=60)
+    viewer.show()
